65648da4d3877476b1ae6a301f66a22cf27fd4cf
chatsopond <42887325+chatsopond@users.noreply.github.com>
2018-09-03 05:47:45 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,41 +1,21 @@
 <?php
 
-$try_contents = array(
-  'type' => 'text',
-  'text' => 'hello'
-);
+$ch = curl_init();
 
-$try_body = array(
-  'type' => 'box',
-  'layout' => 'horizontal',
-  'contents' => [$try_content]
-);
+curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/bot/message/push");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n  \"to\": \"Ubb56c39799ccd5aac622bb1d4fa067c0\",\n  \"messages\": [\n    {\n      \"type\": \"flex\",\n      \"altText\": \"This is a Flex Message\",\n      \"contents\": {\n        \"type\": \"bubble\",\n        \"body\": {\n          \"type\": \"box\",\n          \"layout\": \"horizontal\",\n          \"contents\": [\n            {\n              \"type\": \"text\",\n              \"text\": \"Hello,\"\n            },\n            {\n              \"type\": \"text\",\n              \"text\": \"Flex!\"\n            }\n          ]\n        }\n      }\n    }\n  ]\n}");
+curl_setopt($ch, CURLOPT_POST, 1);
 
-$body_data_contents = array(
-  'type' => 'bubble',
-  'body' => $try_body
-);
+$headers = array();
+$headers[] = "Content-Type: application/json";
+$headers[] = "Authorization: Bearer {HIYMMjaV2M5wSa4JeL5+qUL/prKeGy3Vc5Gj9orWJXrAI7uNNw25xzcOj4xRSfirmX0Z/9bUv0WGJU7eKXl1WDvQ3D2FaiNpND7Fcw1ytKZD963CbXdIgd5k00nC5kTW4TfM52ZmGUJZ8mSl/v23PQdB04t89/1O/w1cDnyilFU=}";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$body_data = array(
-    'type' => 'text',
-    'altText' => 'This is a Flex Message',
-    'contents' => $body_data_contents
-);
-
-$ja = array(
-  'to' => 'Ubb56c39799ccd5aac622bb1d4fa067c0',
-  'message' => [$body_data]
-);
-
-// Configure cURL
-$image_curl = curl_init();
-curl_setopt($image_curl, CURLOPT_URL, "https://api.line.me/v2/bot/message/push");
-curl_setopt($image_curl, CURLOPT_POST, true); // Use POST
-curl_setopt($image_curl, CURLOPT_POSTFIELDS, $body_data); // Setup post body
-curl_setopt($image_curl, CURLOPT_RETURNTRANSFER, true); // Receive server response
-
-// Execute request and read responce
-$session_response = curl_exec($image_curl);
-$response = json_decode($session_response);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
 
 ?>
