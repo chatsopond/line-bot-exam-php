dba251a251b9de4eb1a16d7df5ab0d20ac6c19b0
chatsopond <42887325+chatsopond@users.noreply.github.com>
2018-09-03 05:42:14 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+$try_contents = array(
+  'type' => 'text',
+  'text' => 'hello'
+);
+
+$try_body = array(
+  'type' => 'box',
+  'layout' => 'horizontal',
+  'contents' => [$try_content]
+);
+
+$body_data_contents = array(
+  'type' => 'bubble',
+  'body' => $try_body
+);
+
+$body_data = array(
+    'type' => 'text',
+    'altText' => 'This is a Flex Message',
+    'contents' => $body_data_contents
+);
+
+// Configure cURL
+$image_curl = curl_init();
+curl_setopt($image_curl, CURLOPT_URL, "https://api.line.me/v2/bot/message/push");
+curl_setopt($image_curl, CURLOPT_POST, true); // Use POST
+curl_setopt($image_curl, CURLOPT_POSTFIELDS, $body_data); // Setup post body
+curl_setopt($image_curl, CURLOPT_RETURNTRANSFER, true); // Receive server response
+
+// Execute request and read responce
+$session_response = curl_exec($image_curl);
+$response = json_decode($session_response);
+
+?>
